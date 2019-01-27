de9727b684498d92f74082a0ea3d2b9c31c4d51b
Dmitriy_Kravets <Dmitriy_Kravets@EPKZKARW0542.mshome.net>
2019-01-27 16:11:16 +0600

    Testing static method of a dog class without instance of a dog object itself

--- Hello world/src/Dog.java
+++ Hello world/src/Dog.java
@@ -3,4 +3,7 @@ public class Dog {
 	public Dog() {
 		// TODO Auto-generated constructor stub
 	}
+	public static void gav () {
+		System.out.println("GAV-GAV-GAV!");
+	}
 }
--- Hello world/src/HelloWorld.java
+++ Hello world/src/HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-	}
+		Dog.gav(); //testing static method of a dog class without instance of dog object itself
+	}	
 
 }
