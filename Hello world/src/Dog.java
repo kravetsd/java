1924e8ec9999fe0f61a0ed4dab8bcf549b27a888
Dmitriy_Kravets <Dmitriy_Kravets@EPKZKARW0542.mshome.net>
2019-01-27 19:38:47 +0600

    Tried substring method for String type

--- Hello world/src/Dog.java
+++ Hello world/src/Dog.java
@@ -1,8 +1,9 @@
 
 public class Dog {
-	public Dog() {
+	public Dog(String name) {
 		// TODO Auto-generated constructor stub
 	}
+	
 	public static void gav () {
 		System.out.println("GAV-GAV-GAV".concat(" !!! "));
 	}
--- Hello world/src/HelloWorld.java
+++ Hello world/src/HelloWorld.java
@@ -2,7 +2,10 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		Dog.gav(); //testing static method of a dog class without instance of dog object itself
+		Dog mydog = new Dog("Bill");
+		mydog.gav(); //testing static method of a dog class without instance of dog object itself
+		String name = "Bill";
+		System.out.println(name.substring(1, 3));
 	}	
 
 }
