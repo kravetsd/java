569d2e4eff6ca08bc8e903dd50ae649a96d1da62
Dmitriy_Kravets <Dmitriy_Kravets@EPKZKARW0542.mshome.net>
2019-01-29 00:33:50 +0600

    Implemented static initializaton block for Dog class.

--- Hello world/src/Dog.java
+++ Hello world/src/Dog.java
@@ -1,13 +1,33 @@
+import java.awt.SystemColor;
 
 public class Dog {
-	static String name;
+	static int dognum;
+	static {
+		newDogBorn(); ///static method which gives a message on class creation and increments dognum.
+	}
+	
+	String name;
 	String age;
+	
+
+	
+	static void newDogBorn() {
+		String message = "A new dog was born!";
+		System.out.println(message);
+		dognum += 1;
+	}
+	
+	public Dog (String dogname) {
+		name = dogname;
+	}
+	
 	public Dog(String dogname, String dogage) { //assign name and age to an appropriate fields on an dog instance creation
 		// TODO Auto-generated constructor stub
 		name = dogname;
 		age = dogage;
+		
 	}	
-	public static void gav () {
-		System.out.println("Dog name is "+name.concat(" !!! "));
+	public static void gav (Dog dogobj) {
+		System.out.println("Dog name is "+ dogobj.name);
 	}
 }
--- Hello world/src/HelloWorld.java
+++ Hello world/src/HelloWorld.java
@@ -1,28 +1,16 @@
+import java.awt.SystemColor;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
 		Dog mydog = new Dog("Bill","12");
-		System.out.println(mydog.age);
-		Dog.gav(); //testing static method of a dog class without instance of dog object itself
+		System.out.println(Dog.dognum);
 		Car mycar = new Car();
 		mycar.cardescription();
-		int vim = mycar.getVim();
-		System.out.println(vim);
-		String mycardescr = mycar.getDescr();
-		System.out.println(mycardescr);
 		
-		
-		// generating a new car:
-		Car newmycar = new Car();
-		newmycar.cardescription();
-		int vimnewmycar = newmycar.getVim();
-		System.out.println(vimnewmycar);
-		
+
 		
 		// call static method parkMyCar:
 		Car.parkMyCar("A-24", mycar);
-		
 	}	
 
 }
