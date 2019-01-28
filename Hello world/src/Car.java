d1d9136f97ad3f536dfe63de57979838736912c9
Dmitriy_Kravets <Dmitriy_Kravets@EPKZKARW0542.mshome.net>
2019-01-28 23:25:31 +0600

    Was implemented and called static method with parameters.

--- Hello world/src/Car.java
+++ Hello world/src/Car.java
@@ -1,11 +1,13 @@
 public class Car {
 	String color = "blue"; //in line initialization
 	String type;
+	static int vim;
 	
 	//start of an initialization block
 	{
 		color = "green";
 		type = "sedan";
+		vim += 1;
 	}
 	 
 	//clear constructor
@@ -22,8 +24,19 @@ public class Car {
 	void cardescription() {
 		System.out.println("This is a car "+type+" wth a color "+ color);
 	}
+	
+	int getVim() {
+		return vim;
+	}
+	
 	String getDescr() {
 		String descr = "This is a car "+type+" wth a color "+ color;
 		return descr;
+	}
+	
+	// implementing static method with parameters:
+	static String parkMyCar(String CarPlace, Car anyCar) {
+		System.out.println(anyCar.color+" " +anyCar.type + " was parked to "+ CarPlace + " place");
+		return CarPlace;		
 	}
 }
--- Hello world/src/HelloWorld.java
+++ Hello world/src/HelloWorld.java
@@ -7,8 +7,22 @@ public class HelloWorld {
 		Dog.gav(); //testing static method of a dog class without instance of dog object itself
 		Car mycar = new Car();
 		mycar.cardescription();
+		int vim = mycar.getVim();
+		System.out.println(vim);
 		String mycardescr = mycar.getDescr();
 		System.out.println(mycardescr);
+		
+		
+		// generating a new car:
+		Car newmycar = new Car();
+		newmycar.cardescription();
+		int vimnewmycar = newmycar.getVim();
+		System.out.println(vimnewmycar);
+		
+		
+		// call static method parkMyCar:
+		Car.parkMyCar("A-24", mycar);
+		
 	}	
 
 }
