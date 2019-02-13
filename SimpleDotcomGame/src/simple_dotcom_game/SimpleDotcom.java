f677a796107efea3a5f6dd8cd18a368cef07cd0a
Dmitriy_Kravets <Dmitriy_Kravets@EPKZKARW0542.mshome.net>
2019-02-14 00:29:15 +0600

    Simple dotcom game is implemented.

--- SimpleDotcomGame/src/simple_dotcom_game/SimpleDotcom.java
+++ SimpleDotcomGame/src/simple_dotcom_game/SimpleDotcom.java
@@ -1,5 +1,26 @@
 package simple_dotcom_game;
 
 public class SimpleDotcom {
+	int [] locationcells;
+	int numofhit = 0;
+	
+	void setLocationcells(int [] loc) {
+		locationcells = loc;
+	}
+	String checkYourself(String gues) {
+		int num = Integer.parseInt(gues);
+		for (int cell : locationcells) {
+			if (cell==num) {				
+				if (numofhit == locationcells.length) {
+					return "KILL!";
+				}
+				numofhit++;
+				return "HIT!";
+			}
+		}
+		numofhit++;
+		return "MISS";
+	}
+	
 
 }
--- SimpleDotcomGame/src/simple_dotcom_game/SimpleDotcomGame.java
+++ SimpleDotcomGame/src/simple_dotcom_game/SimpleDotcomGame.java
@@ -1,10 +1,22 @@
 package simple_dotcom_game;
+import java.util.Scanner;
 
 public class SimpleDotcomGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int [] locs = {2,5,7};
+		String usergues = "5";
+		SimpleDotcom dotcom = new SimpleDotcom();
+		dotcom.setLocationcells(locs);
+		String result = dotcom.checkYourself(usergues);
+		String testReuslt = "Failed!";
+		if (result.equals("HIT!")) {
+			testReuslt = "Passed!";
+			
+		}
+		System.out.println(testReuslt);
+		
 	}
 
 }
