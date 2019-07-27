8cfcd555e8dc3730adf9ee289a8ac41597776f1d
Dmitriy Kravets <Dmitriy_Kravets@epam.com>
2019-07-27 11:41:52 +0600

    tested generics. added TODO. Please check

--- src/TestGenerics.java
+++ src/TestGenerics.java
@@ -2,14 +2,18 @@ import com.sun.jndi.url.iiopname.iiopnameURLContextFactory;
 import subclasses.*;
 public class TestGenerics {
     //1. comparing two persons using simple generics and interface _comparable:
+    //TODO We need to add one more field for example "weight" and the next test should contain comparison fo both
+    // "age" and "weight" field.
 
 
     public static void main(String[] args) {
         person m = new person(25);
         person n = new person( 25);
+        m.setName("Dmitriy");
         if (m.compareto(n) ==0) {
             System.out.println("the same age");
         }
         else System.out.println("not equal age");
+        System.out.println(m.getName());
     }
 }
--- src/subclasses/person.java
+++ src/subclasses/person.java
@@ -2,7 +2,7 @@ package subclasses;
 
 public class person implements _comparable<person> {
     private int age;
-
+    private  String name;
 
     public int getAge() {
         return age;
@@ -16,7 +16,22 @@ public class person implements _comparable<person> {
         this.age = age;
     }
 
+    public String getName() {
+        if (name == null){
+            System.out.println( "Please check if your name is OK with name conventions.");
+        //checking if statement in getters an setters.
+        return "nullname";}//TODO Here we need to test throwing an exceptions!!! and exit code not 0 for example.
+        else return name;
+    }
 
+    //testing constructor with if/else statements.
+    public void setName(String name) {
+        if (name.contains("z")) {
+            this.name = name;
+        } else {
+            System.out.println("Your name is not OK with name convention");
+        }
+    }
 
     public int compareto (person obj){
         if (age > obj.age) {return 1;}
