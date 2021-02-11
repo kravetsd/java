76c971f045b55eb74f387d7f151e24e5bd9bfd6c
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-11 12:31:26 +0300

    [java mc]: playing with debugging in intelij idea.

--- JavaMasterClass/HelloWorld/src/Hello.java
+++ JavaMasterClass/HelloWorld/src/Hello.java
@@ -2,5 +2,14 @@ public class Hello {
 
     public static void main ( String[] args){
         System.out.println("Hello World");
+        String st = getString();
+        System.out.println("A new string will be printed");
+        System.out.println(st);
+
+    }
+    public static String getString(){
+        String someStr;
+        someStr = "this is a new string in memory for debugging";
+        return someStr;
     }
 }
