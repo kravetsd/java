431bc13cf63e7f5a1b33948ab7bd5b9d9a431874
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-11 10:17:48 +0300

    Added HelloWrold java masterclass.

--- katas/array_average_streams.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class array_average_streams{
-
-  public static int getAverage(int[] marks){
-    return (int) Arrays.stream(marks).average().orElse(Double.NaN);
-  }
-
-}
