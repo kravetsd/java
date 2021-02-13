cb86c98f688d3e70246832bdef39694323ecd963
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-13 16:37:37 +0300

    [Java Masterclass]: played with operators and expressions.

--- JavaMasterClass/OperatorsAndExpressions/src/com/javams/Main.java
+++ JavaMasterClass/OperatorsAndExpressions/src/com/javams/Main.java
@@ -1,5 +1,7 @@
 package com.javams;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -21,5 +23,23 @@ public class Main {
 
         result = result % 3;
         System.out.println("4 % 3 = "+result);
+
+        result++; //1 + 1 = 2
+        System.out.println("1 + 1 = "+result);
+
+        result--; //2 - 1 = 1
+        System.out.println( "2 - 1 ="+result);
+
+        // result=result + 2;
+        result +=2;
+        System.out.println("1 + 2 = " +result);
+
+        // result = result * 10
+        result*= 10;
+        System.out.println("3 * 10 ="+result);
+
+        // result = result / 3
+        result /=3;
+        System.out.println("30 / 3 = " + result );
     }
 }
