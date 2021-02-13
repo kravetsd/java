c6ba0a9d2149b45753db5fcf2d5ff9ee393727c9
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-13 15:55:38 +0300

    [Java Masterclass] Added boolean and char.

---
+++ JavaMasterClass/CharAndBoolean/src/com/javams/Main.java
@@ -0,0 +1,21 @@
+package com.javams;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myRusChar = 'Ъ';
+        char myEngChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myUnicodeChar);
+        System.out.println(myEngChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+
+        // Boolean
+        boolean isRightAnswer = true;
+
+    }
+}
