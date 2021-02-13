afc0514ea35e9c01d2ce9ea3c8ddfad8e5a435cb
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-13 09:42:17 +0300

    [Java Masterclass] Kilograms to pounds converter.

--- JavaMasterClass/FloatAndDouble/src/com/javams/Main.java
+++ JavaMasterClass/FloatAndDouble/src/com/javams/Main.java
@@ -14,10 +14,28 @@ public class Main {
         System.out.println("Max Double Value is = " + myMaxDoubleValue);
         System.out.println("Min Double Value is = " + myMinDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = (float) 5.25;
-        double myDoubleValue = 5.25;
+        int myIntValue = 5/3;
+        float myFloatValue = (float) 5.25/3f;
+        double myDoubleValue = 5.25/3f;
 
         System.out.println("INT = "+myIntValue+" FLOAT = "+myFloatValue+" DOUBLE = "+myDoubleValue);
+
+
+        // challenge for float and double. We will confer pounds to kilograms:
+
+        double kilograms;
+        double onePoundToKilo = 0.45359237;
+        int pounds = 180;
+
+        kilograms = pounds*onePoundToKilo;
+        System.out.println(pounds+" pounds is equal to "+ kilograms+ " kilograms");
+
+        double oneKiloToPound;
+        oneKiloToPound = pounds/kilograms;
+        System.out.println("One kilogram is equal to "+ oneKiloToPound + " pounds");
+
+        System.out.println("So if my weight is 125 killograms which is equal to "+ 125*oneKiloToPound + " pounds.");
+
+
     }
 }
