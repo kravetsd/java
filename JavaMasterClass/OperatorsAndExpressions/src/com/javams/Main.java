634f1fe0ff850f2c07b5cdbc7c5a37a8129966ee
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-14 11:53:11 +0300

    [Java Masterclass]: played with if then.

--- JavaMasterClass/OperatorsAndExpressions/src/com/javams/Main.java
+++ JavaMasterClass/OperatorsAndExpressions/src/com/javams/Main.java
@@ -41,5 +41,36 @@ public class Main {
         // result = result / 3
         result /=3;
         System.out.println("30 / 3 = " + result );
+
+        boolean isAlien = true;
+        if (isAlien == false)
+            System.out.println("it is not an alien");
+        System.out.println("And I am scared");
+
+        boolean isWithCodeBlock = true;
+        if (isWithCodeBlock){
+            System.out.println("this if then statement is with");
+            System.out.println("code block");
+        }
+
+        int topScore = 100;
+        if (topScore >= 100) {
+            System.out.println("Topscore is greater then or equal tp 100");
+            System.out.println("All other possible variants: " +
+                    "greater > \n" +
+                    "lower < \n" +
+                    "not equesl != \n" +
+                    "less then or equal to <= \n" +
+                    "grater then or equal to >= \n" +
+                    "is equal to ==");
+        }
+        if (topScore >= 100 && topScore<120){
+            System.out.println("Topscore is greater then or equal to 100 AND less then 120");
+        }
+
+        int thirdTopScore = 60;
+        if (thirdTopScore<50 && thirdTopScore!=61 || thirdTopScore==60 && thirdTopScore <50){
+            System.out.println( "ThirdTopScore is less then 50 and not equal to 61 OR it is equal to 60");
+        }
     }
 }
