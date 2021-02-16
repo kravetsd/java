9a42435a1eb9d763af59ff0ec0538f2f8d5d393a
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-16 20:18:48 +0300

    [Java masterclass] Tried ternary operator.

---
+++ JavaMasterClass/ternaryOperator/src/com/javams/Main.java
@@ -0,0 +1,16 @@
+package com.javams;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean isCar = true;
+        boolean wasCar = isCar ? true : false ;
+
+        if (wasCar) {
+            System.out.println("This was a car");
+        } else {
+            System.out.println("This wasn't a cat");
+        }
+    }
+}
