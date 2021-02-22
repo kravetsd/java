6fd35f555a66761eb4ac60bb38d6a1e84adfc7ab
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-22 21:43:59 +0300

    [Java masterclass] MegaBytesConverter class added.

---
+++ JavaMasterClass/MegaBytes Converter/src/com/javams/Main.java
@@ -0,0 +1,10 @@
+package com.javams;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(-1);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(0);
+    }
+}
