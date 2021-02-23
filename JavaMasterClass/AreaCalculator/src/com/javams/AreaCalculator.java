88a231c9685ad3f5af99255ee3655ef982292e5a
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-23 16:18:43 +0300

    [Methods overloading] Area calculator added.

---
+++ JavaMasterClass/AreaCalculator/src/com/javams/AreaCalculator.java
@@ -0,0 +1,18 @@
+package com.javams;
+
+public class AreaCalculator {
+    public static double area(double radius){
+        if (radius<0) {
+            return -1d;
+        }
+        double area = radius*radius*Math.PI;
+        return area;
+    }
+    public static double area(double x, double y){
+        if (x<0 || y<0) {
+            return -1d;
+        }
+        double area = x*y;
+        return area;
+    }
+}
