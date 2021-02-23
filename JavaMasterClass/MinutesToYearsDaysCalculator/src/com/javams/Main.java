c462e5815b6dc2e1e4ca28ce9f9c691f5a4fff61
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-23 19:51:27 +0300

    [Java methods] Minutes to days converter.

---
+++ JavaMasterClass/MinutesToYearsDaysCalculator/src/com/javams/Main.java
@@ -0,0 +1,11 @@
+package com.javams;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MinutesToYearsDaysCalculator.printYearsAndDays(1234589);
+        MinutesToYearsDaysCalculator.printYearsAndDays(527040);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1440);
+    }
+}
