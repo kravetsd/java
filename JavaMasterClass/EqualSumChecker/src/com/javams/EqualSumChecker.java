edd9e71391c67b037c48823b64920db5d2960251
Kravets, Dmitriy <kdykrg@gmail.com>
2021-02-23 10:46:40 +0300

    [Java Masterclass] Added EqualSumChecker class.

---
+++ JavaMasterClass/EqualSumChecker/src/com/javams/EqualSumChecker.java
@@ -0,0 +1,7 @@
+package com.javams;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int firstParam, int secondParam, int thirdParam){
+        return firstParam+secondParam == thirdParam;
+    }
+}
