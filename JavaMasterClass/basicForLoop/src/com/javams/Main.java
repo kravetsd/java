1ca99f23e76f01368368e7a940bc7fff41fea7a7
Kravets, Dmitriy <kdykrg@gmail.com>
2021-03-01 21:52:25 +0300

    [For loop] 3 and 5 challenge.

---
+++ JavaMasterClass/basicForLoop/src/com/javams/Main.java
@@ -0,0 +1,14 @@
+package com.javams;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i=0; i < 10; i++){
+            System.out.println("Hello from for loop step "+i);
+        }
+        for (int j=10; j>0; j--){
+            System.out.println("Hello from decrementing for loop step "+ j);
+        }
+    }
+}
