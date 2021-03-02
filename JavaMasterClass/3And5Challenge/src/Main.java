dd81562469f5111b5912ad0fd2187ce5ef2262bf
Kravets, Dmitriy <kdykrg@gmail.com>
2021-03-02 10:00:06 +0300

    [For loop] Odd sum challenge.

---
+++ JavaMasterClass/3And5Challenge/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("3 is odd number is : "+SumOddRange.isOdd(2));
+        System.out.println(SumOddRange.sumOdd(1,100));
+        System.out.println(SumOddRange.sumOdd(13,13));
+        System.out.println(SumOddRange.sumOdd(100,-100));
+    }
+}
---
+++ JavaMasterClass/3And5Challenge/src/SumOddRange.java
@@ -0,0 +1,22 @@
+public class SumOddRange {
+    public static boolean isOdd(int number){
+        if (number<0) {
+            return false;
+        }
+        return number%2!=0;
+    }
+    public static int sumOdd(int start, int end){
+        int sum = 0;
+        if ((end < start)|| (end<0 || start<0)) {
+            return -1;
+        }
+        for (int i=start; i<=end; i++){
+            if (isOdd(i)){
+                //System.out.println("Found odd = "+i); //Added some debugging;
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
+
