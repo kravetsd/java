f0bddde1392155e5132cbeb47574ab6841321692
Kravets, Dmitriy <kdykrg@gmail.com>
2021-03-12 23:03:54 +0300

    [Algorithms] Flour pack problem RESOLVED.

--- JavaMasterClass/StringUserInput/src/com/javams/FlourPacker.java
+++ JavaMasterClass/StringUserInput/src/com/javams/FlourPacker.java
@@ -2,11 +2,24 @@ package com.javams;
 
 public class FlourPacker {
     public static boolean canPack (int bigCount, int smallCount, int goal){
+        int looper = goal;
+        int tmpBigCount = bigCount;
         if (bigCount<0 || smallCount<0 || goal<0){
             return false;
         }
+        if (goal==5 || goal==1){
+            return true;
+        }
         if ((bigCount*5+smallCount*1)>=goal ){
-            if (goal%5==0 && goal/5>=bigCount){
+            while (looper>5){
+                if (tmpBigCount>0){
+                    tmpBigCount-=1;
+                    looper-=5;
+                } else {
+                    break;
+                }
+            }
+            if (looper<=smallCount){
                 return true;
             }
         }
--- JavaMasterClass/StringUserInput/src/com/javams/Main.java
+++ JavaMasterClass/StringUserInput/src/com/javams/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        parseValueFromString.numberAsString("1234650");
+        //parseValueFromString.numberAsString("1234650");
+        System.out.println(FlourPacker.canPack(2,10,18));
+        System.out.println(FlourPacker.canPack(0,5,5));
     }
 }
