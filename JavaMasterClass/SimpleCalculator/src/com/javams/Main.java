eec32cf22606cef70de38b41bb59bdfb9aa0ffcc
Kravets, Dmitriy <kdykrg@gmail.com>
2021-03-17 17:46:05 +0300

    [Java MS] Simple Class, getters and setters.Person class.

--- JavaMasterClass/SimpleCalculator/src/com/javams/Main.java
+++ JavaMasterClass/SimpleCalculator/src/com/javams/Main.java
@@ -1,8 +1,44 @@
 package com.javams;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        SimpleCalculator calc = new SimpleCalculator();
+        //initializing calculator:
+        calc.setFirstNumber(45);
+        calc.setSecondNumber(23);
+
+        double add1 = calc.getFirstNumber();
+        double add2 = calc.getSecondNumber();
+        System.out.println(add1+"+"+add2+"="+calc.getAdditionResult());
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Initializing a person....");
+        Person man = new Person();
+
+        System.out.println("... plese enter person's first name: ");
+        String name = scanner.nextLine();
+        man.setFirstName(name);
+        System.out.println("first name is "+man.getFirstName());
+
+        System.out.println("... plese enter person's last name: ");
+        String surname = scanner.nextLine();
+        man.setLastName(surname);
+        System.out.println("last name is "+man.getLastName());
+
+        System.out.println("... plese enter person's age: ");
+        int age=scanner.nextInt();
+        man.setAge(age);
+
+        scanner.close();
+
+        System.out.println("Person was initialized:");
+        System.out.println("Full name: "+man.getFullName()+", age: "+man.getAge());
+
+
+
 
     }
 }
