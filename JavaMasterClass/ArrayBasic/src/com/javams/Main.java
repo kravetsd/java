9456d9a1f9b7666a1ccac1b015db12ec5ebbcdd5
Kravets, Dmitriy <kdykrg@gmail.com>
2021-04-01 22:36:54 +0300

    [Java mc] Basic array creation tested. Average calculated.

--- JavaMasterClass/ArrayBasic/src/com/javams/Main.java
+++ JavaMasterClass/ArrayBasic/src/com/javams/Main.java
@@ -1,26 +1,45 @@
 package com.javams;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner;
 
     public static void main(String[] args) {
-	// write your code here
-        int [] intarray;
-        intarray = new int[10]; // created array with 10 spaces for elements;
-        int [] myarrvar = new  int[20]; // the same but in the one line;
+        scanner = new Scanner(System.in);
+        //System.out.println("Please add a number :");
+        //int [] intArray = getIntegers(5);
 
-        for (int i = 0; i < myarrvar.length; i++){
-            myarrvar[i] = i;
-        }
-
-        for (int j : myarrvar) {
-            System.out.println(j);
-        }
-
-        int [] intArray = {1,2,3,4,5,6,7,8,9,0}; // only when we first time define an array!
-
+        int [] intArray = getIntegersFromTerminal(5);
         printArray(intArray);
+        System.out.println("An average is : "+getAverage(intArray));
+    }
+    public static double[] getIntegers(int count){
+        double[] myarr = new double[count];
+        for (int i=0; i<count; i++){
+            myarr[i] = i*5;
+        }
+        scanner.close();
+        return myarr;
     }
 
+    public static double getAverage(int[] array){
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum+=array[i];
+        }
+        return (double) sum/array.length;
+    }
+
+    public static int[] getIntegersFromTerminal(int number){
+        System.out.println("Please, enter "+number+" values to form an array :");
+        int[] myarr = new int[number];
+        for (int i=0; i<number; i++){
+            myarr[i] = scanner.nextInt();
+        }
+        return myarr;
+
+    }
     public static void printArray(int[] array){
         for (int i=0;i<array.length;i++){
             System.out.println(array[i]);
