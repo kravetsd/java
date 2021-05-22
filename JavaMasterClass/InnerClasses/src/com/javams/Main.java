121a277aed698fb958c3de5513416820cd4de353
Kravets, Dmitriy <kdykrg@gmail.com>
2021-05-22 22:53:01 +0300

    [Local  classes] basics..

--- JavaMasterClass/InnerClasses/src/com/javams/Main.java
+++ JavaMasterClass/InnerClasses/src/com/javams/Main.java
@@ -1,19 +1,56 @@
 package com.javams;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
+
 
     public static void main(String[] args) {
-        Gearbox maclaren = new Gearbox(6);
-        maclaren.operateClutch(true);
-        maclaren.changeGear(1);
-        maclaren.operateClutch(false);
-        System.out.println(maclaren.wheelSpeed(1000));
-        maclaren.operateClutch(true);
-        maclaren.changeGear(2);
-        maclaren.operateClutch(false);
-        System.out.println(maclaren.wheelSpeed(1000));
+
+        class ClickListener implements Button.OnClickListener {
+
+            public ClickListener(){
+                System.out.println("I have been attached");
+            }
+            @Override
+            public void onClick(String title) {
+                System.out.println(title+" was clicked");
+            }
+        }
+
+        btnPrint.setOnClickListener(new ClickListener());
+        listen();
+//        Gearbox maclaren = new Gearbox(6);
+////        maclaren.operateClutch(true);
+////        maclaren.changeGear(1);
+////        maclaren.operateClutch(false);
+////        System.out.println(maclaren.wheelSpeed(1000));
+////        maclaren.operateClutch(true);
+////        maclaren.changeGear(2);
+////        maclaren.operateClutch(false);
+////        System.out.println(maclaren.wheelSpeed(1000));
 //        Gearbox.Gear first = maclaren. new Gear(1,12.3);
 //        System.out.println(first.driveSpeed(1000));
 
+    }
+    public static void listen(){
+        boolean quit = false;
+
+        while (!quit){
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+                    break;
+            }
+        }
     }
 }
